0b66e14547964dccae8f34046d7243e04a610046
Alexandros Kalais <scur1343@int4.local.snellius.surf.nl>
2025-03-13 11:29:03 +0100

    Add the checkpoints for SLURM check

--- Final assignment/jobscript_slurm.sh
+++ Final assignment/jobscript_slurm.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=18
 #SBATCH --gpus=1
-#SBATCH --partition=gpu_a100
-#SBATCH --time=00:30:00
+#SBATCH --partition=gpu
+#SBATCH --time=01:30:00
 
 srun apptainer exec --nv --env-file .env container.sif /bin/bash main.sh
